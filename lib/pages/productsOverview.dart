2de1688659df175404212072f9b1140c03307c95
joeyyy688 <edinjoey@gmail.com>
2021-05-07 16:06:20 +0000

    first commit

---
+++ lib/pages/productsOverview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/models/productsModels.dart';
+
+class ProductsOverview extends StatefulWidget {
+  @override
+  _ProductsOverviewState createState() => _ProductsOverviewState();
+}
+
+class _ProductsOverviewState extends State<ProductsOverview> {
+  final List<Products> productsOverview = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
